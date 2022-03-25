71a1d4b7d4c9e72c0a8eaee5170937500bf23147
J Semidey <mtsemidey@gmail.com>
2022-03-25 12:27:44 -0400

    pseudo code in script file

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * WHEN user enters (valid) city name on SUBMIT
+ * ADD city to #history
+ * save to localStorage
+ * create button with name
+ * button handler resubmits GET for display
+ * GET city:
+ * weather icon (weather.icon)
+ * temperature (main.temp)
+ * humidity % (main.humidity)
+ * wind speed (wind.speed)
+ * UV index (current.uvi)
+ * DISPLAY in #currentForecast
+ * GET city:
+ * 5 day forecast ()
+ * DISPLAY in #fiveDayForecast
+ */
+
+// variables
+
+// api key and url variables
+
+// form submit handler
+
+// save search inputs to localstorage
+
+// button click handler (for history buttons)
+
+// get city name
+
+// display content
+
+// event listeners
