3f1f95cd7d9863d46fae6c55d727768c55bc8ca6
J Semidey <mtsemidey@gmail.com>
2022-03-29 12:51:08 -0400

    refactored currentConditions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,59 +5,72 @@ var fiveDayContainerEl = $("#fiveDayForecast");
 var citySearchEl = $("#city-search");
 var cityEl = $("#city-input");
 var cityNameEl = $("#cityName");
-var dateSpan = $("#date");
 var currentDate = moment().format("L");
 
 // api key variable
 var apiKey = "a96c9b1d9614e04af3a4f5f32e7c7b3e"
 
+// function to handle server side errors
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+};
+
+// function to generate 5 day forecast
 var getFiveDayForecast = function(latitude, longitude) {
     var lat = latitude;
     var long = longitude;
 
+    // format future forecast api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apiKey;
 
     fetch(apiUrl).then((response) =>{
-        response.json().then((data) => {
-            // console.log(data);
-            for (var i = 2; i < data.list.length; i+=8) {
-                var forecastDateString = moment(data.list[i].dt_txt).format("L");
-                // console.log(forecastDateString);
-
-                var forecastCol = $("<div class='col-12 col-md-6 col-lg forecast-day mb-3 mt-3'>");
-                var forecastCard = $("<div class='card bg-light shadow'>");
-                var forecastCardBody = $("<div class='card-body'>");
-                var forecastDate = $("<h5 class='card-title'>");
-                var forecastIcon = $("<img>");
-                var forecastTemp = $("<p class='card-text mb-0'>");
-                var forecastWind = $("<p class='card-text mb-0'>");
-                var forecastHumidity = $("<p class='card-text mb-0'>");
-
-
-                fiveDayContainerEl.append(forecastCol);
-                forecastCol.append(forecastCard);
-                forecastCard.append(forecastCardBody);
-
-                forecastCardBody.append(forecastDate);
-                forecastCardBody.append(forecastIcon);
-                forecastCardBody.append(forecastTemp);
-                forecastCardBody.append(forecastWind);
-                forecastCardBody.append(forecastHumidity);
-                
-                forecastIcon.attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
-                forecastIcon.attr("alt", data.list[i].weather[0].main);
-                forecastDate.text(forecastDateString);
-                forecastTemp.text(data.list[i].main.temp);
-                forecastTemp.prepend("Temp: ");
-                forecastTemp.append("&deg;F");
-                forecastWind.text(data.list[i].wind.speed);
-                forecastWind.prepend("Wind: ");
-                forecastWind.append(" MPH");
-                forecastHumidity.text(data.list[i].main.humidity);
-                forecastHumidity.prepend("Humidity: ");
-                forecastHumidity.append(" %");
-            }
-        })
+        if (response.ok) {
+            response.json().then((data) => {
+                // console.log(data);
+                for (var i = 2; i < data.list.length; i+=8) {
+                    var forecastDateString = moment(data.list[i].dt_txt).format("L");
+                    // console.log(forecastDateString);
+    
+                    var forecastCol = $("<div class='col-12 col-md-6 col-lg forecast-day mb-3 mt-3'>");
+                    var forecastCard = $("<div class='card bg-light shadow'>");
+                    var forecastCardBody = $("<div class='card-body'>");
+                    var forecastDate = $("<h5 class='card-title'>");
+                    var forecastIcon = $("<img>");
+                    var forecastTemp = $("<p class='card-text mb-0'>");
+                    var forecastWind = $("<p class='card-text mb-0'>");
+                    var forecastHumidity = $("<p class='card-text mb-0'>");
+    
+    
+                    fiveDayContainerEl.append(forecastCol);
+                    forecastCol.append(forecastCard);
+                    forecastCard.append(forecastCardBody);
+    
+                    forecastCardBody.append(forecastDate);
+                    forecastCardBody.append(forecastIcon);
+                    forecastCardBody.append(forecastTemp);
+                    forecastCardBody.append(forecastWind);
+                    forecastCardBody.append(forecastHumidity);
+                    
+                    forecastIcon.attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+                    forecastIcon.attr("alt", data.list[i].weather[0].main);
+                    forecastDate.text(forecastDateString);
+                    forecastTemp.text(data.list[i].main.temp);
+                    forecastTemp.prepend("Temp: ");
+                    forecastTemp.append("&deg;F");
+                    forecastWind.text(data.list[i].wind.speed);
+                    forecastWind.prepend("Wind: ");
+                    forecastWind.append(" MPH");
+                    forecastHumidity.text(data.list[i].main.humidity);
+                    forecastHumidity.prepend("Humidity: ");
+                    forecastHumidity.append(" %");
+                }
+            })
+        } else {
+            handleErrors();
+        }
     })
 
 }
@@ -72,33 +85,56 @@ var getCurrentConditions = function(latitude, longitude){
     fetch(apiUrl).then((response) => {
         if (response.ok) {
             response.json().then((data) => {
-                    // console.log(data);
-                    var currentConditionsDiv = $("<div class='bg-light shadow px-3 py-3'>");
-                    currentConditionsDiv.append(currentDate);
-
-                    var currentTemp = $("<p>")
-
-                    var temp = data.current.temp;
-                    var humidity = data.current.humidity;
-                    var windSpeed = data.current.wind_speed;
-                    var uvIndex = data.current.uvi;
+                    console.log(data);
+                    // create new DOM elements
+                    var currentConditionsDiv = $("<div class='card bg-light shadow px-3 py-3'>");
+                    var currentConditionsCardBody = $("<div class='card-body'>");
+                    var currentConditionsDate = $("<h5 class='card-title'>");
+                    currentConditionsDate.text(currentDate);
+                    var currentConditionsIcon = $("<img>");
+                    var currentConditionsTemp = $("<p class='card-text mb-0'>");
+                    var currentConditionsHumidity = $("<p class='card-text mb-0'>");
+                    var currentConditionsWind = $("<p class='card-text mb-0'>");
+                    var currentConditionsUvIndex = $("<p class='card-text mb-0'>");
+                    // icon code from data array
                     var iconcode = data.current.weather[0].icon;
-                    // console.log(temp);
-                    // console.log(humidity);
-                    // console.log(windSpeed);
-                    // console.log(uvIndex);
-                    
-                    // display in dom
-                    // var currentConditionsDiv = document.createElement("div");
 
-                    // currentConditionsDiv.setAttribute("class", "bg-light shadow px-3 py-3");
-                    currentConditionsDiv.innerHTML =
-                    "<img src='https://openweathermap.org/img/w/" + iconcode + ".png' alt='" + data.current.weather[0].description +"' /><em> " +
-                    "</em><p><strong>Temperature:</strong> " + temp + 
-                    "</p><p><strong>Humidity:</strong> " + humidity +
-                    "</p><p><strong>Wind Speed:</strong> " + windSpeed +
-                    "</p><p><strong>UV Index:</strong> " + uvIndex + "</p>";
+                    // append new div to container, append new card to new div
                     currentForecastContainerEl.append(currentConditionsDiv);
+                    currentConditionsDiv.append(currentConditionsCardBody);
+                    
+                    // append new elements to card body
+                    currentConditionsCardBody.append(currentConditionsDate);
+                    currentConditionsCardBody.append(currentConditionsIcon);
+                    currentConditionsCardBody.append(currentConditionsTemp);
+                    currentConditionsCardBody.append(currentConditionsHumidity);
+                    currentConditionsCardBody.append(currentConditionsWind);
+                    currentConditionsCardBody.append(currentConditionsUvIndex);
+                    
+
+                    // icon attributes
+                    currentConditionsIcon.attr("src", "https://openweathermap.org/img/w/" + iconcode + ".png");
+                    currentConditionsIcon.attr("alt", data.current.weather[0].description);
+                    currentConditionsIcon.attr("title", data.current.weather[0].description);
+
+                    // temp attributes
+                    currentConditionsTemp.text(data.current.temp);
+                    currentConditionsTemp.prepend("Temp: ");
+                    currentConditionsTemp.append("&deg;F");
+
+                    // humidity attributes
+                    currentConditionsHumidity.text(data.current.humidity);
+                    currentConditionsHumidity.prepend("Humidity: ");
+                    currentConditionsHumidity.append(" %");
+
+                    // wind attributes
+                    currentConditionsWind.text(data.current.wind_speed);
+                    currentConditionsWind.prepend("Wind: ");
+                    currentConditionsWind.append(" MPH");
+
+                    // uv index attributes
+                    currentConditionsUvIndex.text(data.current.uvi);
+                    currentConditionsUvIndex.prepend("UV Index: ");
                 });
             } else {
                 handleErrors();
